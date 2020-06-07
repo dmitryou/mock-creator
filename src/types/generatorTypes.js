c32a848dc82c7aa637fdb938d7e031000e0b3b3a
dmitryou <dmitryourkevich@gmail.com>
2020-06-07 22:47:20 +0300

    types constants

---
+++ src/types/generatorTypes.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    UNIQ: 'uniq',
+    STATIC: 'static',
+    RANDOM: 'random',
+    RANGE: 'range',
+});
---
+++ src/types/valueTypes.js
@@ -0,0 +1,5 @@
+module.exports = Object.freeze({
+    NUMBER: 'number',
+    STRING: 'string',
+    DATE: 'date',
+});
