d250e7e5b82407fbea534c7ad0b2ccb88c3ec7c6
Matan Rokach <mtn.rkh@gmail.com>
2020-06-07 22:45:35 +0300

    wiring things

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,7 @@
 const getDefaultConfiguration = require('./getDefaultConfiguration');
+const getValueByTypes = require('./getValueByTypes');
 
 module.exports = {
   getDefaultConfiguration,
+  getValueByTypes,
 };
